6f395d2a3cebd94c8d9fbaa0605e9411dbf1ee8b
Lucas Duarte Barbosa Arrais de Oliveira <dev_lucas_arrais@MacBook-Pro-de-Lucas.local>
2026-02-20 11:44:39 -0300

    feat:configuranddo prettier

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+
+    entry: ['src'],
+    format: ['esm'],
+    outDir: 'build',
+    sourcemap: true,
+    shims: true,
+    target: 'esnext',
+})
